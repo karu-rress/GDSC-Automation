7afcbdff6a94bf2c55e987fadc948c258d2e875e
Karu <nsun527@naver.com>
2023-05-12 00:45:13 +0900

    Add project files.

---
+++ githubstats/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace githubstats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
